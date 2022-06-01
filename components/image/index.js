4bba90034b089bb1f6e2e7d2687d469af9694773
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-01 15:00:32 -0500

    Create Image component integrating NextImage and TailwindCSS

---
+++ components/image/index.js
@@ -0,0 +1,15 @@
+import NextImage from 'next/image';
+import classNames from 'classnames';
+
+const Image = ({ className, height, src, ...rest }) => {
+  return (
+    <div
+      className={classNames('_image-container', className)}
+      style={height ? { height } : null}
+    >
+      <NextImage className="_image" layout="fill" src={src} {...rest} />
+    </div>
+  );
+};
+
+export default Image;
