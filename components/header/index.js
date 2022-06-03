1bd009e72a942bc4b459614cdcfefc73f6ec0ea7
Hares Mahmood <hmahmood229@gmail.com>
2022-06-03 12:38:51 +0000

    Add back OAuth handlers to `Header` component `button`s.

--- components/header/index.js
+++ components/header/index.js
@@ -21,12 +21,16 @@ const Header = () => {
                 <div className="flex items-center space-x-6">
                     <button
                         className="text-gray-600 hover:text-gray-800"
+                        type="button"
+                        href="/api/auth/signin"
+                        onClick={onSignInClick}
                     >
                         Sign in
                     </button>
                     <button
                         className="text-gray-900 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium text-sm px-4 py-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                         type="button"
+                        onClick={onSignInClick}
                     >
                         Get Started
                     </button>
@@ -47,9 +51,9 @@ const Header = () => {
                     </div>
                     <p> Hi, <span className="font-semibold">{session.user.name}</span> </p>
                     <a
-                    className="text-gray-600 hover:text-gray-800 text-xs"
-                    href="/api/auth/signout"
-                    onClick={onSignOutClick}
+                        className="text-gray-600 hover:text-gray-800 text-xs"
+                        href="/api/auth/signout"
+                        onClick={onSignOutClick}
                     >
                         Sign out
                     </a>
@@ -62,9 +66,9 @@ const Header = () => {
         <nav className="flex flex-row justify-between w-full bg-white px-8 py-5">
             <div className="flex items-center space-x-3">
                 <Image
-                className="h-7"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                alt="Workflow"
+                    className="h-7"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
                 />
                 <h3 className="font-semibold text-lg"> O<span className="hidden sm:inline">rientation </span>H<span className="hidden sm:inline">ackathon</span> </h3>
             </div>
