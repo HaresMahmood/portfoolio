3429f65b0c03fc393b6539921e818028a04c4649
Hares Mahmood <hmahmood229@gmail.com>
2022-06-03 16:52:22 +0000

    Make Profile page responsive.
    - Using Flexbox wrapping.

--- pages/profile.js
+++ pages/profile.js
@@ -15,48 +15,47 @@ const Profile = () => {
 
             <Header/>
 
-            <main className="flex flex-col items-start gap-32 w-full h-full px-14 py-8">
+            <main className="flex flex-col items-start gap-16 w-full h-full px-14 py-8">
                 <div className="flex flex-row gap-8 w-full">
-                    <div class="bg-gray-900 h-full aspect-square rounded-full"></div>
+                    {/* <div class="bg-gray-900 h-full aspect-square rounded-full"></div> */}
                     <div className="flex flex-col items-start gap-4 w-full">
                         <h1 className="text-4xl font-bold"> <span className="text-indigo-500">John Doe</span> <br/> <span className="text">Software Engineer</span> </h1>
                         <p className="text-gray-500"> john.doe@email.com </p>
                     </div>
                 </div>
 
-                <div className="flex flex-col items-start gap-8 w-full md:flex-row md:gap-48">
-                    <form className="flex flex-col items-start gap-3 w-full" action="/send-data-here" method="post">
-                        <h2 className="text-2xl font-semibold"> General </h2>
-                        <div className="flex flex-row items-center justify-between w-full">
-                            <label className="font-medium text-gray-900" for="name"> Full name </label>
-                            <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="name" name="name" />
+                <form className="flex flex-col items-start gap-8 w-full" action="/send-data-here" method="post">
+                    <div className="flex flex-row flex-wrap items-start gap-x-40 gap-y-8 w-full">
+                        <div className="flex flex-col flex-grow basis-80 items-start gap-3 w-full">
+                            <h2 className="text-2xl font-semibold"> General </h2>
+                            <div className="flex flex-row items-center justify-between w-full">
+                                <label className="font-medium text-gray-900" for="name"> Full name </label>
+                                <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="name" name="name" />                        </div>
+                            <div className="flex flex-row items-center justify-between w-full">
+                                <label className="font-medium text-gray-900 w-28" for="title"> Job title </label>
+                                <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="title" name="title" />
+                            </div>
+                            <div className="flex flex-row items-center justify-between w-full">
+                                <label className="font-medium text-gray-900 w-28" for="about"> About </label>
+                                <textarea className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-3xl bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" rows={3} id="about" name="about" />
+                            </div>
                         </div>
-                        <div className="flex flex-row items-center justify-between w-full">
-                            <label className="font-medium text-gray-900 w-28" for="title"> Job title </label>
-                            <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="title" name="title" />
+                        <div className="flex flex-col flex-grow basis-80 items-start gap-3 w-full min-w-min">
+                            <h2 className="text-2xl font-semibold"> General </h2>
+                            <div className="flex flex-row items-center justify-between w-full">
+                                <label className="font-medium text-gray-900" for="name"> Full name </label>
+                                <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="name" name="name" />                        </div>
+                            <div className="flex flex-row items-center justify-between w-full">
+                                <label className="font-medium text-gray-900 w-28" for="title"> Job title </label>
+                                <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="title" name="title" />
+                            </div>
+                            <div className="flex flex-row items-center justify-between w-full">
+                                <label className="font-medium text-gray-900 w-28" for="about"> About </label>
+                                <textarea className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-3xl bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" rows={3} id="about" name="about" />
+                            </div>
                         </div>
-                        <div className="flex flex-row items-center justify-between w-full">
-                            <label className="font-medium text-gray-900 w-28" for="about"> About </label>
-                            <textarea className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-3xl bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" rows={3} id="about" name="about" />
-                        </div>
-                    </form>
-
-                    <form className="flex flex-col items-start gap-3 w-full" action="/send-data-here" method="post">
-                        <h2 className="text-2xl font-semibold"> General </h2>
-                        <div className="flex flex-row items-center justify-between w-full">
-                            <label className="font-medium text-gray-900" for="name"> Full name </label>
-                            <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="name" name="name" />
-                        </div>
-                        <div className="flex flex-row items-center justify-between w-full">
-                            <label className="font-medium text-gray-900 w-28" for="title"> Job title </label>
-                            <input className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-full bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" type="text" id="title" name="title" />
-                        </div>
-                        <div className="flex flex-row items-center justify-between w-full">
-                            <label className="font-medium text-gray-900 w-28" for="about"> About </label>
-                            <textarea className="appearance-none border-2 border-gray-500 text-gray-500 font-medium rounded-3xl bg-transparent w-2/3 px-4 py-2 focus:text-gray-900" rows={3} id="about" name="about" />
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </main>
 
             <footer className="flex items-center justify-center w-full p-4 border-t">
