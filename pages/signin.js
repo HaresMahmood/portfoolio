f65e8b610d306cd8cb4de3694730e2b89b5877a4
Hares Mahmood <hmahmood229@gmail.com>
2022-06-04 21:46:53 +0000

    Add additional styling to signup-page.

--- pages/signin.js
+++ pages/signin.js
@@ -4,54 +4,53 @@ import Image from '../components/image';
 import Layout from '../components/layout';
 
 const SignIn = ({ providers }) => {
-  return (
-    <Layout>
-      <Head>
-        <title>Sign In</title>
-      </Head>
-      <div className="flex flex-col items-center justify-center w-full p-5">
-        <div className="flex flex-col items-center gap-7 text-center">
-        <Image
-          alt=""
-          className="h-80 w-full p-5"
-          src="/undraw_sign_in_re_o58h.svg"
-        />
+    return (
+        <Layout>
+            <Head>
+                <title>Sign In</title>
+            </Head>
+            <div className="flex w-full flex-col items-center justify-center p-5">
+                <div className="flex flex-col items-center gap-7 text-center">
+                    <Image
+                        alt=""
+                        className="h-80 w-full p-5"
+                        src="/undraw_sign_in_re_o58h.svg"
+                    />
 
-        <div className="flex flex-col gap-1">
-        <h1 className="text-4xl font-bold">
-          Sign in
-        </h1>
-        <p>
-          Connect one of the following accounts to generate your portfolio.
-        </p>
-        </div>
+                    <div className="flex flex-col gap-1">
+                        <h1 className="text-4xl font-bold">Sign in</h1>
+                        <p>
+                            Connect one of the following accounts to generate
+                            your portfolio.
+                        </p>
+                    </div>
 
-        <div className="flex gap-3 justify-center border-t-2 pt-7 w-full">
-          <>
-            {Object.values(providers).map(provider => (
-              <div key={provider.name}>
-                <button
-                  className="rounded-full border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                  onClick={() =>
-                    signIn(provider.id, {
-                      callbackUrl: `${window.location.origin}/profile`
-                    })
-                  }
-                >
-                  {provider.name}
-                </button>
-              </div>
-            ))}
-          </>
-        </div>
-        </div>
-      </div>
-    </Layout>
-  );
+                    <div className="flex w-full justify-center gap-3 border-t-2 pt-7">
+                        <>
+                            {Object.values(providers).map(provider => (
+                                <div key={provider.name}>
+                                    <button
+                                        className="rounded-full border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                                        onClick={() =>
+                                            signIn(provider.id, {
+                                                callbackUrl: `${window.location.origin}/profile`
+                                            })
+                                        }
+                                    >
+                                        {provider.name}
+                                    </button>
+                                </div>
+                            ))}
+                        </>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default SignIn;
 
 export const getServerSideProps = async () => {
-  return { props: { providers: await getProviders() } };
+    return { props: { providers: await getProviders() } };
 };
