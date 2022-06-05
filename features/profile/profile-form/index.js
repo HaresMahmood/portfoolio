3f1abc5558c7a3eba6272acc994b20d098daa80e
Hares Mahmood <hmahmood229@gmail.com>
2022-06-05 20:08:42 +0100

    Move form button to profile hero section.

--- features/profile/profile-form/index.js
+++ features/profile/profile-form/index.js
@@ -17,7 +17,7 @@ const ProfileForm = () => {
             validationSchema={profileFormSchema}
         >
             {({ values }) => (
-                <Form className="flex w-full flex-row flex-wrap items-start gap-x-40 gap-y-12">
+                <Form id="form" className="flex w-full flex-row flex-wrap items-start gap-x-40 gap-y-12">
                     {profileFormTemplate.map(section => (
                         <ProfileFormSection
                             defaultValues={values}
@@ -25,12 +25,6 @@ const ProfileForm = () => {
                             {...section}
                         />
                     ))}
-                    <button
-                        className="rounded-full border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                        type="submit"
-                    >
-                        Save changes
-                    </button>
                 </Form>
             )}
         </Formik>
--- features/profile/profile-hero/index.js
+++ features/profile/profile-hero/index.js
@@ -6,22 +6,40 @@ const ProfileHero = () => {
     const { data: session } = useSession();
 
     return (
-        <div className="flex w-full flex-row items-center gap-8">
-            <Image
-                className="h-32 aspect-square"
-                src={session.user.image}
-                alt=""
-                style={{ borderRadius: '100%' }}
-            />
-            <div className="flex w-full flex-col items-start gap-4">
-                <h1 className="text-4xl font-bold">
-                    <span className="text-indigo-500">
-                        {session?.user?.name}
-                    </span>
-                    <br />
-                    <span className="text">Software Engineer</span>
-                </h1>
-                <p className="text-gray-500">{session.user.email}</p>
+        <div className="flex flex-row flex-wrap items-center justify-between w-full gap-10"> 
+            <div className="flex flex-row items-center flex-grow gap-8">
+                <Image
+                    className="h-32 aspect-square"
+                    src={session.user.image}
+                    alt=""
+                    style={{ borderRadius: '100%' }}
+                />
+                <div className="flex w-full flex-col items-start gap-4">
+                    <h1 className="text-4xl font-bold">
+                        <span className="text-indigo-500">
+                            {session?.user?.name}
+                        </span>
+                        <br />
+                        <span className="text">Software Engineer</span>
+                    </h1>
+                    <p className="text-gray-500">{session.user.email}</p>
+                </div>
+            </div>
+
+            <div className="flex gap-3 w-fit">
+                <button
+                    className="rounded-full border border-gray-300 px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:bg-indigo-500 dark:text-white dark:hover:bg-indigo-400 dark:focus:ring-gray-700"
+                    type="submit"
+                    form="form"
+                >
+                    Save Changes
+                </button>
+                <button
+                    className="rounded-full border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                    type="button"
+                >
+                    View Portfolio
+                </button>
             </div>
         </div>
     );
