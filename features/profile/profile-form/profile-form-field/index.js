617e7562d8eb045c3d8e3c2b10a87d885f885dea
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-05 01:35:24 -0500

    Fix styling issues

--- features/profile/profile-form/profile-form-field/index.js
+++ features/profile/profile-form/profile-form-field/index.js
@@ -11,17 +11,19 @@ const ProfileFormField = ({ label, name, type }) => {
                     >
                         {label}
                     </label>
-                    <Field
-                        as="textarea"
-                        className="w-2/3 appearance-none rounded-3xl border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
-                        name={name}
-                        rows={3}
-                    />
-                    <ErrorMessage
-                        className="text-xs"
-                        name={name}
-                        component="div"
-                    />
+                    <div className="w-2/3">
+                        <Field
+                            as="textarea"
+                            className="w-full appearance-none rounded-3xl border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
+                            name={name}
+                            rows={3}
+                        />
+                        <ErrorMessage
+                            className="text-xs text-red-600"
+                            name={name}
+                            component="div"
+                        />
+                    </div>
                 </>
             );
         case 'period':
@@ -34,27 +36,31 @@ const ProfileFormField = ({ label, name, type }) => {
                         Period
                     </label>
                     <div className="flex w-2/3 flex-row items-center gap-3">
-                        <Field
-                            className="w-3/4 appearance-none rounded-full border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
-                            name={`${name}.from`}
-                            type="month"
-                        />
-                        <ErrorMessage
-                            name={`${name}.from`}
-                            className="text-xs"
-                            component="div"
-                        />
+                        <div className="w-3/4 space-y-1">
+                            <Field
+                                className="w-full appearance-none rounded-full border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
+                                name={`${name}.from`}
+                                type="month"
+                            />
+                            <ErrorMessage
+                                name={`${name}.from`}
+                                className="text-xs text-red-600"
+                                component="div"
+                            />
+                        </div>
                         <div className="h-0.5 w-12 bg-gray-900" />
-                        <Field
-                            className="w-3/4 appearance-none rounded-full border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
-                            name={`${name}.to`}
-                            type="month"
-                        />
-                        <ErrorMessage
-                            name={`${name}.to`}
-                            className="text-xs"
-                            component="div"
-                        />
+                        <div className="w-3/4 space-y-1">
+                            <Field
+                                className="w-full appearance-none rounded-full border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
+                                name={`${name}.to`}
+                                type="month"
+                            />
+                            <ErrorMessage
+                                name={`${name}.to`}
+                                className="text-xs text-red-600"
+                                component="div"
+                            />
+                        </div>
                     </div>
                 </>
             );
@@ -64,16 +70,18 @@ const ProfileFormField = ({ label, name, type }) => {
                     <label className="font-medium" htmlFor={name}>
                         {label}
                     </label>
-                    <Field
-                        className="w-2/3 appearance-none rounded-full border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
-                        name={name}
-                        type={type}
-                    />
-                    <ErrorMessage
-                        className="text-xs"
-                        name={name}
-                        component="div"
-                    />
+                    <div className="w-2/3 space-y-1">
+                        <Field
+                            className="w-full appearance-none rounded-full border-2 border-gray-500 bg-transparent px-4 py-2 font-medium text-gray-500 focus:text-gray-900"
+                            name={name}
+                            type={type}
+                        />
+                        <ErrorMessage
+                            className="text-xs text-red-600"
+                            name={name}
+                            component="div"
+                        />
+                    </div>
                 </>
             );
     }
--- pages/signin.js
+++ pages/signin.js
@@ -1,27 +1,27 @@
 import { getProviders, signIn } from 'next-auth/react';
 
 const SignIn = ({ providers }) => {
-  return (
-    <>
-      {Object.values(providers).map(provider => (
-        <div key={provider.name}>
-          <button
-            onClick={() =>
-              signIn(provider.id, {
-                callbackUrl: `${window.location.origin}/profile`
-              })
-            }
-          >
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
-    </>
-  );
+    return (
+        <>
+            {Object.values(providers).map(provider => (
+                <div key={provider.name}>
+                    <button
+                        onClick={() =>
+                            signIn(provider.id, {
+                                callbackUrl: `${window.location.origin}/profile`
+                            })
+                        }
+                    >
+                        Sign in with {provider.name}
+                    </button>
+                </div>
+            ))}
+        </>
+    );
 };
 
 export default SignIn;
 
 export const getServerSideProps = async () => {
-  return { props: { providers: await getProviders() } };
+    return { props: { providers: await getProviders() } };
 };
