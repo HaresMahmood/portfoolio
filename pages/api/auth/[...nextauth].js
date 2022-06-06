6a0ebbb17189ede097cdaf21bbe6d91b8ea448fc
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-06 11:47:53 -0500

    Remove Facebook provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,3 @@
-import FacebookProvider from 'next-auth/providers/facebook';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import NextAuth from 'next-auth';
@@ -34,10 +33,6 @@ const options = {
         signIn: '/signin'
     },
     providers: [
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_ID,
-            clientSecret: process.env.FACEBOOK_SECRET
-        }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
