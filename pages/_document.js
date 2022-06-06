63ca450f6c44180602382bfdebe8dad3aa9d58c5
Hares Mahmood <hmahmood229@gmail.com>
2022-06-06 16:01:38 +0100

    Add extra properties to font link to ensure proper rendering on Vercel.

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,10 @@ const Document = () => {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
+                    rel="preload"
                     href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200;300;400;500;600;700;800;900&display=swap"
-                    rel="stylesheet"
+                    as="font"
+                    crossOrigin=""
                 />
             </Head>
             <body>
