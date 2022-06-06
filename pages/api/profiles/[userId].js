fe574e0389a6a10f6d5ea744e565c5b9d54fc33b
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-05 20:50:28 -0500

    Enable Education, Experience, and Projects CRUD operations

--- pages/api/profiles/[userId].js
+++ pages/api/profiles/[userId].js
@@ -2,18 +2,30 @@ import prisma from '../../../lib/prisma';
 
 const handler = async (req, res) => {
     const { method } = req;
+    const { userId } = req.query;
 
     switch (method) {
         // Get profile
         case 'GET':
             res.status(200).json(
                 await prisma.profile.findUnique({
-                    where: { userId: req.query.userId }
+                    where: { userId }
+                })
+            );
+            break;
+        // Update profile
+        case 'PUT':
+            const data = req.body;
+
+            res.status(200).json(
+                await prisma.profile.update({
+                    where: { userId },
+                    data
                 })
             );
             break;
         default:
-            res.setHeader('Allow', ['GET']);
+            res.setHeader('Allow', ['GET, PUT']);
             res.status(405).end(`Method ${method} Not Allowed`);
     }
 };
