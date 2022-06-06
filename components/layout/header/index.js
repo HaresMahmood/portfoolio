e8565d854e0b20025b80578c4fa7e7ad8a2ed37c
Hares Mahmood <hmahmood229@gmail.com>
2022-06-06 18:41:52 +0100

    Adjust button styling.

--- components/layout/header/index.js
+++ components/layout/header/index.js
@@ -44,7 +44,7 @@ const Header = () => {
                 </p>
                 <Link href="/api/auth/signout" passHref>
                     <Button
-                        className="text-xs p-1 bg-none text-gray-600 hover:text-gray-800 hover:bg-gray-100"
+                        className="text-xs bg-none outline-none ring-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-100"
                         onClick={onSignOutClick}
                         type="button"
                     >
